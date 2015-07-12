cf23654cbe9a9932da77d60e2fc63f47807c6557
keremdogan <dilingucu@gmail.com>
2015-07-12 06:52:04 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1218,7 +1218,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["SonSarıkBükücü"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
